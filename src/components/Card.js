0ac5861c8027223bacf4f095a1995dd0bea4b149
sakib-xrz <sakibxrz21@gmail.com>
2023-08-02 21:33:31 +0600

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { RxDragHandleHorizontal } from "react-icons/rx";
+
+const Card = ({children}) => {
+    return (
+        <div className="px-5 py-2 my-2 rounded-md cursor-grab">
+            <h1 className="bg-gray-100 p-4 text-xl text-gray-800 flex items-center gap-3 rounded-sm">
+                <span>
+                    <RxDragHandleHorizontal />
+                </span>
+                {children}
+            </h1>
+        </div>
+    );
+};
+
+export default Card;
